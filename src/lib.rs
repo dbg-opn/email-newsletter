6b0724dcfb06160728a2693951bbabcc159654c9
Patcha96 <eryk.grzeskow@gmail.com>
2025-05-02 22:59:08 +0800

    randomised testing port

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,17 @@
 use actix_web::{web, App, HttpServer, HttpResponse, Responder};
+use actix_web::dev::Server;
+use std::net::TcpListener;
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub async fn run() -> Result<(), std::io::Error> {
-    HttpServer::new(|| {
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
         App::new()
             .route("health_check", web::get().to(health_check))
     })
-        .bind("127.0.0.1:8000")?
-        .run()
-        .await
+        .listen(listener)?
+        .run();
+        Ok(server)
 }
