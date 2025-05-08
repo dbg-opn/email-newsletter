6772805812288ce9fec27c7729d3d2b29f31221a
Patcha96 <eryk.grzeskow@gmail.com>
2025-05-09 00:28:18 +0800

    changed telemetry to tracing instead of logging

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
-use env_logger::Env;
+use email_newsletter::telemetry::{get_subscriber, init_subscriber};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    // `init` does call `set_logger`, so this is all we need to do.
-    // We are falling back to printing all logs at info-level or above
-    // if the RUST_LOG environment variable has not been set.
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    let subscriber = get_subscriber("email-newsletter".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,6 +1,7 @@
 use actix_web::{HttpResponse, web};
 use chrono::Utc;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(serde::Deserialize)]
@@ -12,16 +13,32 @@ pub struct FormData {
 // At the start always return 200 OK
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
+    // Spans, like logs, have an associated level
+    // `info_span` creates a span at the info-level
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
+    );
+    // Using `enter` in an async function is a recipe for disaster!
+    // Bear with me for now, but don't do this at home.
+    // See the following section on `Instrumenting Futures`
+    let _request_span_guard = request_span.enter();
+
+    // `_request_span_guard` is dropped at the end of `subscribe`
+    // That's when we "exit" the span
+    tracing::info!(
         "request_id {} - Adding '{}''{}' as a new subscriber.",
         request_id,
         form.email,
         form.name
     );
-    log::info!(
-        "request_id {} - Saving new subscriber details in the database",
-        request_id
-    );
+
+    // We do not call `.enter` on query_span!
+    // `.instrument` takes care of it at the right moments
+    // in the query future lifetime
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -35,17 +52,19 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     // We use `get_ref` to get an immutable reference to the `PgPool`
     // wrapped by `web::Data`.
     .execute(pool.get_ref())
+    // First we attach the instrumentation, then we `.await` it
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!(
+            tracing::info!(
                 "request_id {} - New subscriber details have been saved",
                 request_id
             );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "request_id {} - Failed to execute query: {:?}",
                 request_id,
                 e
