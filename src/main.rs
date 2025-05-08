5c7dbf5b277b044e5128b2b2123e48bab7dae19c
Patcha96 <eryk.grzeskow@gmail.com>
2025-05-08 23:47:49 +0800

    added basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 use email_newsletter::configuration::get_configuration;
 use email_newsletter::startup::run;
-use sqlx::{Connection, PgPool};
+use env_logger::Env;
+use sqlx::PgPool;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    // `init` does call `set_logger`, so this is all we need to do.
+    // We are falling back to printing all logs at info-level or above
+    // if the RUST_LOG environment variable has not been set.
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
